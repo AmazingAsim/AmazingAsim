9d90a0da9e85a33491206cf021fe6d2b7a3b39d1
asim sheikh <amazingAsim3000@gmail.com>
2022-04-28 23:06:53 +0530

    changed theme color

--- views/script.js
+++ views/script.js
@@ -5,7 +5,7 @@ fetch('./projects.json').then((res)=>{return res.json()}).then(r=>
             let grid=document.querySelector('.grid');
             let griditem=document.createElement('div');
             griditem.innerHTML=` <div class="card text-white bg-light text-dark mb-3" style="max-width: 18rem;">
-            <div class="card-header" style="background-color:yellow; color:black;">${p.name}</div>
+            <div class="card-header" style="background-color:var(--purple); color:black;">${p.name}</div>
             <div class="card-body">
               <h5 class="card-title">${p.name}</h5>
               <p class="card-text">${p.desc}.</p>
